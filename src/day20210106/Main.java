180dd606421a0039e26c5c734ec79c50c13354da
Naeun <knae11@naver.com>
2021-01-06 15:49:20 +0900

    bj1076

--- src/day20210106/Main.java
+++ src/day20210106/Main.java
@@ -3,43 +3,42 @@ package day20210106;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 /* 제출용 template
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main{
 	public static void main(String args[]) throws IOException {
 
-	     int[][] remains = {
-            {10},
-            {1},
-            {6, 2, 4, 8},
-            {1, 3, 9, 7},
-            {6, 4},
-            {5},
-            {6},
-            {1, 7, 9, 3},
-            {6, 8, 4, 2},
-            {1, 9}
-        };
+        Map<String, Integer> resistance = new HashMap<>(Map.of(
+            "black", 0,
+            "brown", 1,
+            "red", 2,
+            "orange", 3,
+            "yellow", 4,
+            "green", 5,
+            "blue", 6,
+            "violet", 7,
+            "grey", 8,
+            "white", 9
+        ));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int ts = Integer.parseInt(br.readLine());
-        for (int i = 0; i < ts; i++) {
-            String[] pair = br.readLine().split(" ");
-            int a = Integer.parseInt(pair[0]) % 10;
-            int b = Integer.parseInt(pair[1]);
-            if (a == 0 || a == 1 || a == 5 || a == 6) {
-                System.out.println(remains[a][0]);
-            } else if (a == 4 || a == 9) {
-                System.out.println(remains[a][b % 2]);
-            } else {
-                System.out.println(remains[a][b % 4]);
-            }
-
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        String input3 = br.readLine();
+        int two = resistance.get(input1) * 10 + resistance.get(input2);
+        if (two == 0) {
+            System.out.println("0");
+        } else {
+            StringBuilder answer = new StringBuilder();
+            answer.append(two);
+            answer.append("0".repeat(resistance.get(input3)));
+            System.out.println(answer);
         }
 
 	}
@@ -77,9 +76,38 @@ public class Main{
         }
     }
 
+    private static void bj1076() throws IOException {
+        Map<String, Integer> resistance = new HashMap<>(Map.of(
+            "black", 0,
+            "brown", 1,
+            "red", 2,
+            "orange", 3,
+            "yellow", 4,
+            "green", 5,
+            "blue", 6,
+            "violet", 7,
+            "grey", 8,
+            "white", 9
+        ));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        String input3 = br.readLine();
+        int two = resistance.get(input1) * 10 + resistance.get(input2);
+        if (two == 0) {
+            System.out.println("0");
+        } else {
+            StringBuilder answer = new StringBuilder();
+            answer.append(two);
+            answer.append("0".repeat(resistance.get(input3)));
+            System.out.println(answer);
+        }
+
+    }
+
 
     public static void main(String[] args) throws IOException {
-        bj1009();
+        bj1076();
     }
 
 
