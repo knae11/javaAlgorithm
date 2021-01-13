3a152ec6c102e142ef14ea11715742d9492ba4d3
Naeun <knae11@naver.com>
2021-01-13 11:07:51 +0900

    leetcode : reverse integer

--- src/day20210113/Main.java
+++ src/day20210113/Main.java
@@ -33,8 +33,39 @@ public class Main {
 }
      */
 
+    public static int reverse(int x) {
+        String stringX = String.valueOf(x);
+        StringBuilder temp = new StringBuilder();
+        if (stringX.charAt(0) == '-'){
+            stringX = stringX.substring(1);
+            temp.append('-');
+        }
+        for(int i = stringX.length()-1; i>-1; i--){
+            temp.append(stringX.charAt(i));
+        }
+        long tempAnswer = Long.parseLong(temp.toString());
+        if(tempAnswer > Integer.MAX_VALUE || tempAnswer < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) tempAnswer;
+    }
+/* 10의 자리 수를 받아오는 다른 풀이 , integer 값 넘는 경우 체크하는 부분 확인!
+(2^21-1) = 2147483647가 max 인데 214748364이면 끝에가 8부터는 overflow!
+-2^31 = -2147483648가 min 인데 214748364이면 끝자리가 -9부터는 overflow!
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+ */
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum( new int[] {1,2,3}, 4)));
+        System.out.println(reverse(-123));
     }
 }
